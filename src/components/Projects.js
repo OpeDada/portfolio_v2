bf8aa9817b45bd4cc945ff73fc1c6862ad04958b
OpeDada <opedada@gmail.com>
2023-10-21 23:26:39 +0900

    created the projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,27 @@
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+
+
+    </section>
+  )
+}
