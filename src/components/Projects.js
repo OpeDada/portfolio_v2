60f3bef6e9bac3a3080ffbf1ba6605f66a23025c
OpeDada <opedada@gmail.com>
2023-10-21 23:37:28 +0900

    added tab functionality

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,56 @@
-export const Projects = () => {
+import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 
+export const Projects = () => {
   const projects = [
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg1,
+      // imgUrl: projImg1,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg2,
+      // imgUrl: projImg2,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg3,
+      // imgUrl: projImg3,
     },
   ];
 
   return (
     <section className="project" id="project">
-
-
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              veniam et quod excepturi est itaque ullam natus quidem laudantium
+              numquam quam quo quos dolor placeat accusamus qui, deleniti
+              reiciendis eum.
+            </p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+            <Nav
+              variant="pills"
+              className="nav-pills mb-5 justify-content-center align-items-center"
+              id="pills-tab"
+            >
+              <Nav.Item>
+                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">Tab 3</Nav.Link>
+              </Nav.Item>
+            </Nav>
+              </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
     </section>
-  )
-}
+  );
+};
