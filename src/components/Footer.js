e084c214aac2f187de7de94216ad282fc9eb5e5f
OpeDada <opedada@gmail.com>
2023-10-24 15:56:43 +0900

    styled footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 // import { MailchimpForm } from "./MailchimpForm";
-// import logo from "../assets/img/logo.svg";
-// import navIcon1 from "../assets/img/nav-icon1.svg";
-// import navIcon2 from "../assets/img/nav-icon2.svg";
-// import navIcon3 from "../assets/img/nav-icon3.svg";
+import logo from "../assets/logo3.png";
+import navIcon1 from "../assets/nav-icon1.svg";
+import navIcon2 from "../assets/github.svg";
+import navIcon3 from "../assets/mail.png";
 
 export const Footer = () => {
   return (
@@ -12,18 +12,18 @@ export const Footer = () => {
         <Row className="align-items-center">
           {/* <MailchimpForm /> */}
           <Col size={12} sm={6}>
-            <img src={""} alt="Logo" />
+            <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="/">
-                <img src={""} alt="Icon" />
+                <img src={navIcon1} alt="Icon" />
               </a>
               <a href="/">
-                <img src={""} alt="Icon" />
+                <img src={navIcon2} alt="Icon" />
               </a>
               <a href="/">
-                <img src={""} alt="Icon" />
+                <img src={navIcon3} alt="Icon" />
               </a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
