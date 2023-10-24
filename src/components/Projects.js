c2815afe813f66a9c5a6ef1060bcb224ff9598ed
OpeDada <opedada@gmail.com>
2023-10-24 12:05:10 +0900

    passed props into project card and included bg img

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import colorSharp2 from "../assets/color-sharp2.png"
+import colorSharp2 from "../assets/color-sharp2.png";
 
 export const Projects = () => {
   const projects = [
@@ -33,45 +33,38 @@ export const Projects = () => {
               numquam quam quo quos dolor placeat accusamus qui, deleniti
               reiciendis eum.
             </p>
-              <Tab.Container id="projects-tabs" defaultActiveKey="first">
-            <Nav
-              variant="pills"
-              className="nav-pills mb-5 justify-content-center align-items-center"
-              id="pills-tab"
-            >
-              <Nav.Item>
-                <Nav.Link eventKey="first">Tab 1</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">Tab 3</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content>
-              <Tab.Pane eventKey="first">
-                <Row>
-                  {
-                    projects.map((project, index) => {
-                      return (
-                        <ProjectCard
-                          key={index}
-                          {...project}
-                        />
-                      )
-                    })
-                  }
-                </Row>
-              </Tab.Pane>
-               <Tab.Pane eventKey="second"></Tab.Pane>
-               <Tab.Pane eventKey="third"></Tab.Pane>
-            </Tab.Content>
-              </Tab.Container>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second"></Tab.Pane>
+                <Tab.Pane eventKey="third"></Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt="bgimg"/>
+      <img className="background-image-right" src={colorSharp2} alt="bgimg" />
     </section>
   );
 };
