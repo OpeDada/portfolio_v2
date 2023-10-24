b0b28a48c74c8179b66908add677f2459120a995
OpeDada <opedada@gmail.com>
2023-10-24 15:49:36 +0900

    created footer section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
