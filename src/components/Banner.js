e76e957019b88669dcab84e40d08ae7505cba2fd
OpeDada <opedada@gmail.com>
2023-10-25 23:27:38 +0900

    added trackvisibility tag to contact section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
                 >
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    {`Hi! I'm Judy`}{" "}
+                    {`Hi! I'm Ope`}{" "}
                     <span
                       className="txt-rotate"
                       dataPeriod="1000"
