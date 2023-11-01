dc92a0f49880f410796277cfef0a6054c0c6ecb1
OpeDada <opedada@gmail.com>
2023-11-01 21:16:41 +0900

    edited gmail code

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "opedada@gmail.com",
-    pass: "pyft uiwj ryyd wute",
+    pass: "pyftuiwjryydwute",
   },
 });
 
