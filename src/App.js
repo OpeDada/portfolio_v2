0d0719e3d87f96baf4cadce2d869e32ec851b3bf
OpeDada <opedada@gmail.com>
2023-11-06 23:24:00 +0900

    created fnc for switching between themes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { createContext, useState } from 'react';
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  }
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme}}>
+      <div className="App" id={theme}>
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
