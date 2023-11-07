1123cd8dbc361784010b8ba0d70845df311633a9
OpeDada <opedada@gmail.com>
2023-11-07 22:42:53 +0900

    styled the switch label

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import { Skills } from './components/Skills';
-import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import { createContext, useState } from 'react';
-import ReactSwitch from 'react-switch';
+import "./App.css";
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState("dark")
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"))
-  }
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme}}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
-        <div className='switch'>
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
           <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
         </div>
         <NavBar />
