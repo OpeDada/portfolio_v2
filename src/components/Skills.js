56da91b8907373fd1bffc51f69f5b2d6a446190d
OpeDada <opedada@gmail.com>
2023-11-11 14:24:04 +0900

    added skills images

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/images/meter1.svg"
-import meter2 from "../assets/images/meter2.svg"
-import meter3 from "../assets/images/meter3.svg"
 import colorSharp from "../assets/images/color-sharp.png";
 
 export const Skills = () => {
@@ -30,41 +27,70 @@ export const Skills = () => {
       <Container>
         <Row>
           <Col>
-          <div className="skill-bx">
-            <h2>
-              Skills
-            </h2>
-          <Carousel responsive={responsive} infinite={true} className="skill-slider">
-            <div className="item">
-              {/* <img src={meter1} alt="carousel" /> */}
-              <h5>Javascript</h5>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                <div className="item">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
+                    alt="carousel"
+                  />
+                  <h5>Javascript</h5>
+                </div>
+                <div className="item">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgnL9jZgMNaHVvVbppBGJUNcgnVwAMTWKFVBygOCvV2GIWiSIl8gbrBK2hTatbA7WxoZQ&usqp=CAU"
+                    alt="carousel"
+                  />
+                  <h5>React</h5>
+                </div>
+                <div className="item">
+                  <img
+                    src="https://thequantuminsider.com/wp-content/uploads/github.png"
+                    alt="carousel"
+                  />
+
+                  <h5>Github</h5>
+                </div>
+                <div className="item">
+                  <img
+                    src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/Next.js%20Series%20-%20Event%20icon-01.png"
+                    alt="carousel"
+                  />
+                  <h5>NextJS</h5>
+                </div>
+                <div className="item">
+                  <img
+                    src="https://static-00.iconduck.com/assets.00/sql-icon-2048x2028-ow2ma2t9.png"
+                    alt="carousel"
+                  />
+                  <h5>SQL</h5>
+                </div>
+
+                <div className="item">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/2048px-HTML5_Badge.svg.png"
+                    alt="carousel"
+                  />
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_2dfsPeW1sL0lDikNWEnlRvo1-k-KtEVp39aUTc9hzzDlbgj-ZhxRVXkK2p1tGCevIkY&usqp=CAU"
+                    alt="carousel"
+                  />
+                  <h5>CSS</h5>
+                </div>
+              </Carousel>
             </div>
-            <div className="item">
-              {/* <img src={meter2} alt="carousel" /> */}
-              <h5>React</h5>
-            </div>
-            <div className="item">
-              {/* <img src={meter3} alt="carousel" /> */}
-              <h5>NextJS</h5>
-            </div>
-            <div className="item">
-              <h5>SQL</h5>
-            </div>
-            <div className="item">
-              <h5>Github</h5>
-            </div>
-            <div className="item">
-              <h5>HTML</h5>
-            </div>
-            <div className="item">
-              <h5>CSS</h5>
-            </div>
-          </Carousel>
-          </div>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-left" src={colorSharp} alt="bg-img"/>
+      <img className="background-image-left" src={colorSharp} alt="bg-img" />
     </section>
-  )
+  );
 };
