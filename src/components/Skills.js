ca97b8e3832758d189f0babb895ed1be27cb9ea9
OpeDada <opedada@gmail.com>
2023-11-11 13:12:40 +0900

    adjusted general design

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,7 @@ import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/images/meter1.svg"
 import meter2 from "../assets/images/meter2.svg"
 import meter3 from "../assets/images/meter3.svg"
-import colorSharp from "../assets/images/color-sharp.png"
+import colorSharp from "../assets/images/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
