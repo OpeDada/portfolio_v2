a5c3c7609a0e1389b785804f039c1393ef986e61
OpeDada <opedada@gmail.com>
2023-11-14 22:55:55 +0900

    debugging project img styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import colorSharp2 from "../assets/images/color-sharp2.png";
-import projImg1 from "../assets/images/bead2.png";
-import projImg2 from "../assets/images/project-img2.png";
-import projImg3 from "../assets/images/project-img3.png";
+import projImg1 from "../assets/images/p1.png";
+import projImg2 from "../assets/images/p2.png";
+import projImg3 from "../assets/images/p3.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
