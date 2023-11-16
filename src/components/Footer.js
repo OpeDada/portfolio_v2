d3950876a63158631d7569a56b13911f7b1d122d
OpeDada <opedada@gmail.com>
2023-11-16 23:01:39 +0900

    made footer links active

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,29 +8,43 @@ import { Link, BrowserRouter as Router } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          {/* <MailchimpForm /> */}
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="/">
-                <img src={navIcon1} alt="Icon" />
-              </a>
-              <a href="/">
-                <img src={navIcon2} alt="Icon" />
-              </a>
-              <a href="/">
-                <img src={navIcon3} alt="Icon" />
-              </a>
-            </div>
-            <p>Copyright 2023. All Rights Reserved</p>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+    <Router>
+      <footer className="footer">
+        <Container>
+          <Row className="align-items-center">
+            {/* <MailchimpForm /> */}
+            <Col size={12} sm={6}>
+              <img src={logo} alt="Logo" />
+            </Col>
+            <Col size={12} sm={6} className="text-center text-sm-end">
+              <div className="social-icon">
+                <Link
+                  to="https://www.linkedin.com/in/opemipo-dada"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={navIcon1} alt="" />
+                </Link>
+                <Link
+                  to="https://github.com/OpeDada"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={navIcon2} alt="" />
+                </Link>
+                <Link
+                  to="mailto:opedada@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={navIcon3} alt="" />
+                </Link>
+              </div>
+              <p>Copyright 2023. All Rights Reserved</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </Router>
   );
 };
